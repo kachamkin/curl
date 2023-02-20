5ca63af0bd68d10f0afcdc745a06dc4b343b5500
Andrey Kachamkin <kachamkin@gmail.com>
2023-02-20 15:40:38 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,19 +1,18 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-
 #include <cstdlib>
 #include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <algorithm>
+#include <future>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/asio/ssl.hpp>
-//#include <boost/asio/ssl/error.hpp>
-//#include <boost/asio/ssl/stream.hpp>
 #include <QMainWindow>
 
 using namespace  std;
@@ -22,6 +21,11 @@ namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 namespace ssl = boost::asio::ssl;
+
+void rtrim(string&);
+void ltrim(string&);
+void toLower(string&);
+void trim(string&);
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -41,7 +45,6 @@ public slots:
 private:
     Ui::MainWindow *ui;
     void HttpRequest(const std::string&, const std::string&, const std::string&, const std::unordered_map<string,string>&);
-
 };
 
 #endif // MAINWINDOW_H
